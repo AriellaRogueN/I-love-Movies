82b8cd9b7d800bf022d9794a5c6eacc50f2a2924
Helvette <malastrugo@live.cl>
2018-01-10 14:11:51 -0300

    actualización de search, click a see more

--- assets/js/search.js
+++ assets/js/search.js
@@ -20,11 +20,11 @@ var generateInfo = function(code){
 			*/
 			var html = "<div class='row movie vertical-align'>"+
 						"<span class='code'>" + imdbID + "</span>" +
-						"<div class='col-md-3 img-container text-center'>"+
+						"<div class='col-md-3 col-xs-12 img-container text-center'>"+
 						"<img src="+ poster +"alt='Movie'>"+
 					"</div>"+
-					"<div class='col-md-8 info-container'>"+
-						"<h4 class=title>"+ title + "</h4>"+
+					"<div class='col-md-8 col-xs-12 info-container'>"+
+						"<h4 class=title>"+ title + "</h4>"+ "<span class='more'>see more</span>" +
 						"<span class='main'>"+
 							"<span class='bold'>Director: </span><span class='director'>"+ director + "</span>"+
 						"</span>"+
@@ -38,11 +38,12 @@ var generateInfo = function(code){
 						"<p class='short-plot'><span class='bold'>Plot: </span>"+ plot + "</p>"+
 						"<hr class='last-hr'>"+
 					"</div>"+
-					"<div class='col-md-1 icons-container'>"+
+					"<div class='col-md-1 col-xs-12 icons-container'>"+
 						"<ul>"+
 							"<li class='heart not-active'><i class='far fa-heart fa-2x'></i></li>"+
 							"<li class='garbage not-active'><i class='far fa-trash-alt fa-2x'></i></li>"+
 							"<li class='see-later not-active'><i class='far fa-clock fa-2x'></i></li>"+
+							"<li class='check not-active'><i class='far fa-check-circle fa-2x'></i></li>"+
 						"</ul>"
 					"</div>"
 				"</div>";
@@ -51,6 +52,7 @@ var generateInfo = function(code){
 			//console.log("Movie not found!");
 		}
 	})
+
 }
 /*
 * ejemplo Sci-Fi, ejemplo sin Sci-Fi
@@ -78,14 +80,94 @@ var generateResults = function(movieInput){
 			}
 		}
 	})
-	// **** PONER AQUÍ COSA
+	/*
+	* interacción con el contenido generado e insertado anteriormente ***********
+	*/
+	setTimeout(function() {
+		$(".more").click(function(){
+			/*
+			* extrayendo código imdb para insertar datos en el perfil de la página
+			*/
+			console.log($(this).parents()[1]);
+			var div = $(this).parents()[1];
+			div = $(div).children()[0];
+			console.log(div);
+			var code = $(div).text();
+			console.log(code);
+		})
+		/*
+		* interacción con los íconos
+		*/
+		$(".heart").click(function(){
+			if($(this).hasClass("not-active")){
+				$(this).removeClass("not-active");
+				// qué hacer con la película en caso de que le demos click al corazón vacío
+			}else{
+				$(this).addClass("not-active");
+				// qué hacer con la película en caso de que le demos click al corazón lleno
+			}
+
+			if($(this).hasClass("not-active")){
+				$(this).html("<i class='far fa-heart fa-2x'></i>");
+			}else{
+				$(this).html("<i class='fas fa-heart fa-2x'></i>");
+			}
+		})
+
+		$(".garbage").click(function(){
+			if($(this).hasClass("not-active")){
+				$(this).removeClass("not-active");
+				// qué hacer con la película en caso de que le demos click al garbage vacío
+			}else{
+				$(this).addClass("not-active");
+				// qué hacer con la película en caso de que le demos click al garbage lleno
+			}
+			if($(this).hasClass("not-active")){
+				$(this).html("<i class='far fa-trash-alt fa-2x'>");
+			}else{
+				$(this).html("<i class='fas fa-trash-alt fa-2x'>");
+			}
+		})
+
+		$(".see-later").click(function(){
+			if($(this).hasClass("not-active")){
+				$(this).removeClass("not-active");
+				// qué hacer con la película en caso de que le demos click al reloj vacío
+			}else{
+				$(this).addClass("not-active");
+				// qué hacer con la película en caso de que le demos click al reloj lleno
+			}
+
+			if($(this).hasClass("not-active")){
+				$(this).html("<i class='far fa-clock fa-2x'></i>");
+			}else{
+				$(this).html("<i class='fas fa-clock fa-2x'>");
+			}
+		})
+
+		$(".check").click(function(){
+			if($(this).hasClass("not-active")){
+				$(this).removeClass("not-active");
+				// qué hacer con la película en caso de que le demos click al check vacío
+			}else{
+				$(this).addClass("not-active");
+				// qué hacer con la película en caso de que le demos click al check lleno
+			}
+
+			if($(this).hasClass("not-active")){
+				$(this).html("<i class='far fa-check-circle fa-2x'></i>");
+			}else{
+				$(this).html("<i class='fas fa-check-circle fa-2x'></i>");
+			}
+		})
+	}, 1500);
 }
 // generateResults("interstellar");
 
 /*
 * función de click al btn enter
 */
-$(".btn-search").click(function(){
+$(".btn-search").click(function onClick(){
 	if($(".movie-input").val().length == 0){
 		
 	}
@@ -144,7 +226,24 @@ $(".see-later").click(function(){
 	}
 })
 
+$(".check").click(function(){
+	if($(this).hasClass("not-active")){
+		$(this).removeClass("not-active");
+		// qué hacer con la película en caso de que le demos click al corazón vacío
+	}else{
+		$(this).addClass("not-active");
+		// qué hacer con la película en caso de que le demos click al corazón
+	}
+
+	if($(this).hasClass("not-active")){
+		$(this).html("<i class='far fa-check-circle fa-2x'></i>");
+	}else{
+		$(this).html("<i class='fas fa-check-circle fa-2x'></i>");
+	}
+})
+
 
 $(".movies-container .title").click(function(){
-	alert("hola");
+	alert("título: " + $(this).text());
 })
+
